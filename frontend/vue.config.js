929a8446087d8bedce64b50eccf3513eaea4a026
Simon Jäger <simon-jaeger@users.noreply.github.com>
2021-01-28 01:55:10 +0100

    j

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  devServer: {open: true, proxy: "http://hack-online.test"},
+  devServer: {proxy: "http://hackonline.test", disableHostCheck: true},
   outputDir: "../backend/public/",
 }
+
+// NOTE: access dev server via localhost.test (register as custom hostname for 127.0.0.1) to avoid ajax lag
+// --> see: https://www.dionysopoulos.me/improve-php-performance-on-windows.html
